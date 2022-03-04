64662cef5cb5c3a724a055a1d3e65440ee9ac2c4
great-jin <great__jin@163.com>
2022-03-04 15:32:21 +0800

    拦截器

--- healthcare-frontend/src/router/index.js
+++ healthcare-frontend/src/router/index.js
@@ -1,16 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/views/index'
+import Test from '@/views/test/index'
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // 1. 判断是不是登录页面
+  // 是登录页面
+  if(to.path === '/') {
+    next()
+  } else {
+    // 不是登录页面
+    // 2. 判断 是否登录过
+    let token = localStorage.getItem('token')
+    token ? next() : next('/')
+  }
+})
+
+export default router
+
+
---
+++ healthcare-frontend/src/views/test/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
