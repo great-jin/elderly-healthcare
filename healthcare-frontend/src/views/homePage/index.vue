57c21eeebb73f54cad9b82fa6ec1289495deeb57
great-jin <great__jin@163.com>
2022-03-09 15:44:01 +0800

    tab

--- healthcare-frontend/src/views/homePage/index.vue
+++ healthcare-frontend/src/views/homePage/index.vue
@@ -49,12 +49,12 @@
         style="background-color: white"
         collapsible
       >
-        <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="() => (collapsed = !collapsed)"
-        />
         <a-menu theme="light" mode="inline" :default-selected-keys="['1']">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="() => (collapsed = !collapsed)"
+          />
           <a-menu-item key="1" @click="routePage('monitor')">
             <a-icon type="area-chart" />
             <span>数据监控</span>
@@ -73,24 +73,27 @@
           </a-menu-item>
           <a-menu-item key="5" @click="routePage('logs')">
             <a-icon type="file-protect" />
-            <span>任务分配</span>
+            <span>系统日志</span>
           </a-menu-item>
         </a-menu>
       </a-layout-sider>
 
       <a-layout style="height: 94%">
-        <!--
         <a-layout-header style="background: #fff; padding: 0">
-          <a-tabs type="editable-card" hide-add style="margin: 10px 25px 10px 25px">
-            <a-tab-pane
-              v-for="pane in tabData"
-              :key="pane.key"
-              :tab="pane.title"
-              :closable="pane.closable"
-            />
+          <a-tabs v-model="activeKey"
+                  type="editable-card"
+                  @edit="onEdit"
+                  style="margin: 10px 10px 0px 10px"
+                  hide-add
+          >
+            <a-tab-pane v-for="pane in panes"
+                        :key="pane.key"
+                        :tab="pane.title"
+                        :closable="pane.closable"
+            >
+            </a-tab-pane>
           </a-tabs>
         </a-layout-header>
-        -->
         <a-layout-content
           :style="{ margin: '16px 24px', padding: '24px', background: '#fff'}"
           style="overflow: auto;"
@@ -104,21 +107,24 @@
 
 <script>
 export default {
-  name: "Home",
-  inject: ['reload'],
+  name: "Service",
   data() {
+    const panes = [
+      { title: '数据监控', content: 'Content of Tab 1', key: '1', closable: false }
+    ]
     return{
       id: '',
       collapsed: false,
-      tabTitle: '',
-      tabData: [],
-      tabIndex: 0
+      activeKey: panes[0].key,
+      panes,
+      newTabIndex: 0
     }
   },
   mounted() {
     this.id = this.$route.query.id
-    this.routePage('staff')
-    window.addEventListener('beforeunload', e => this.beforeunloadHandler(e))
+    this.routePage('monitor')
+    // 监控页面关闭
+    // window.addEventListener('beforeunload', e => this.beforeunloadHandler(e))
   },
   methods:{
     beforeunloadHandler(e) {
@@ -139,26 +145,64 @@ export default {
           break
       }
     },
-    routePage(data) {
-      const activeKey = this.tabIndex++;
-      switch (data){
-        case 'chart':
-          this.$router.push('/service/chart')
-          break
-        case 'monitor':
-          this.tabData.push({
-            title: `数据监控`,
+    onEdit(targetKey, action) {
+      this[action](targetKey);
+    },
+    remove(targetKey) {
+      let activeKey = this.activeKey;
+      let lastIndex;
+      this.panes.forEach((pane, i) => {
+        if (pane.key === targetKey) {
+          lastIndex = i - 1;
+        }
+      });
+      const panes = this.panes.filter(pane => pane.key !== targetKey);
+      if (panes.length && activeKey === targetKey) {
+        if (lastIndex >= 0) {
+          activeKey = panes[lastIndex].key;
+        } else {
+          activeKey = panes[0].key;
+        }
+      }
+      this.panes = panes;
+      this.activeKey = activeKey;
+    },
+    tabJudge(data) {
+      this.panes.forEach((pane, i) => {
+        if (pane.title !== data) {
+          const panes = this.panes
+          this.panes = []
+          const activeKey = `tab${this.newTabIndex++}`
+          panes.push({
+            title: `${data}`,
+            content: 'Content of new Tab',
             key: activeKey
-          })
+          });
+          this.panes = panes
+          this.activeKey = activeKey
+        }
+      })
+    },
+    routePage(data) {
+      switch (data){
+        case 'monitor':
+          this.tabJudge('数据监控')
           this.$router.push('/service/monitor')
           break
+        case 'chart':
+          this.tabJudge('数据展示')
+          this.$router.push('/service/chart')
+          break
         case 'access':
+          this.tabJudge('入住登记')
           this.$router.push('/service/access')
           break
         case 'patient':
+          this.tabJudge('病人管理')
           this.$router.push('/service/patient')
           break
         case 'logs':
+          this.tabJudge('系统日志')
           this.$router.push('/service/logs')
           break
       }
