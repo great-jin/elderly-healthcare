0f4e30c199eea113221bb886b27197f95a76971a
great <great__jin@163.com>
2022-03-18 22:08:42 +0800

    动态表单

--- healthcare-frontend/src/views/storePage/storageManage/index.vue
+++ healthcare-frontend/src/views/storePage/storageManage/index.vue
@@ -1,17 +1,174 @@
 <template>
-  <div>
-    <h1>药品管理</h1>
+  <div style="background-color: white;">
+    <a-row>
+      <a-form
+        :form="form"
+        :label-col="labelCol"
+        :wrapper-col="wrapperCol"
+      >
+        <a-form-item label="上报计划" class="required">
+          <a-button type="link" style="margin-right: 30px" @click="addReport">新增</a-button>
+          <a-button type="link" style="margin-right: 30px" @click="ok">完成</a-button>
+        </a-form-item>
+        <div
+          v-for="(item, index) in dynamicInfo"
+          :key="index"
+          style="margin-bottom: 15px; margin-left: 50px"
+        >
+          <a-row>
+            <a-col :span="2" />
+            <a-col :span="7">
+              <a-form-item
+                label="上报周期"
+                :label-col="formItemLayout.labelCol"
+                :wrapper-col="formItemLayout.wrapperCol"
+                :prop="`dynamicInfo[${index}].selectReportPeriod`"
+                class="required"
+              >
+                <a-select
+                  placeholder="请选择上报周期"
+                  v-decorator="[
+                    `${item}.reportPeriod`,
+                    { rules: [{ required: false, message: '上报周期不能为空' }] }
+                  ]"
+                >
+                  <a-select-option value="年">年</a-select-option>
+                  <a-select-option value="月">月</a-select-option>
+                  <a-select-option value="日">日</a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+            <a-col :span="7">
+              <a-form-item
+                label="上报时间"
+                :label-col="formItemLayout.labelCol"
+                :wrapper-col="formItemLayout.wrapperCol"
+                :prop="`dynamicInfo[${index}].reportPeriod`"
+                class="required"
+              >
+                <a-select
+                  placeholder="请选择上报时间"
+                  v-decorator="[
+                    `${item}.reportDate`,
+                    { rules: [{ required: false, message: '上报时间不能为空' }] }
+                  ]"
+                  class="required"
+                >
+                  <a-select-option value="每月10号">每月10号</a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+            <a-icon
+              class="dynamic-delete-button"
+              type="minus-circle-o"
+              @click="() => remove(item)"
+            />
+          </a-row>
+          <a-row>
+            <a-col :span="2" />
+            <a-col :span="12">
+              <a-form-item
+                label="上报单位"
+                :wrapper-col="formItemLayout.wrapperCol"
+                :prop="`dynamicInfo[${index}].orgList[]`"
+                class="required"
+              >
+                <a-input
+                  v-decorator="[
+                    `${item}.orgName`,
+                    { rules: [{ required: false, message: '上报单位不能为空' }] }
+                  ]"
+                  style="width: 100%"
+                  tree-checkable
+                />
+              </a-form-item>
+            </a-col>
+            <a-col>
+              <a-button type="link" @click="openOrgModal(index, item.uuid)">选择</a-button>
+              <orgModal ref="orgModal" @getOrg="getModalValues" />
+            </a-col>
+          </a-row>
+        </div>
+      </a-form>
+    </a-row>
   </div>
 </template>
 
 <script>
+import orgModal from './orgModal'
+
 export default {
-  data() {
+  name: 'index',
+  components: {
+    orgModal
+  },
+  data () {
     return {
+      dynamicInfo: [
+        {
+          reportPeriod: '',
+          reportDate: '',
+          orgName: ''
+        }
+      ],
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 },
+      formItemLayout: {
+        labelCol: { span: 7 },
+        wrapperCol: { span: 14 }
+      },
+      form: this.$form.createForm(this)
+    }
+  },
+  methods: {
+    openOrgModal (index, data) {
+      this.$refs.orgModal[index].paramReceive(data)
+    },
+    getModalValues (pram) {
+      console.log(pram)
+    },
+    ok () {
+      this.form.validateFields((errors, values) => {
+        if (!errors) {
+          console.log(values)
+        }
+      })
+    },
+    cancel () {
+      this.dynamicInfo = []
+      this.form.resetFields()
+    },
+    guid () {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0
+        var v = c === 'x' ? r : (r & 0x3 | 0x8)
+        return v.toString(16)
+      })
+    },
+    addReport () {
+      this.dynamicInfo.push({
+        uuid: this.guid(),
+        reportPeriod: '',
+        reportDate: '',
+        orgName: ''
+      })
+    },
+    remove (item) {
+      console.log('item', item)
+      this.dynamicInfo = this.dynamicInfo.filter(op => (op.uuid !== item.uuid))
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
+  .required .ant-form-item-label label::before {
+    content: '*';
+    line-height: 1;
+    display: inline-block;
+    margin-right: 4px;
+    color: #f5222d;
+    font-size: 14px;
+    font-family: SimSun, sans-serif;
+  }
 </style>
